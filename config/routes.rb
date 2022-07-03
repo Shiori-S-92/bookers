f5eb1f84b107b07a50039e6627a8d6b18c0781f2
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-07-03 08:21:50 +0000

    [Update] Create layout for index page of book

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'top' => 'homes#top'
+  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'books' => 'books#books'
+  get '/books' => 'books#index'
+  post '/books' => 'books#create'
+  get '/books' => 'books#index'
+  get '/books/:id' => 'books#show', as: 'book'
+  get '/books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => "books#update", as: 'update_book'
+  delete 'lists/:id' => 'books#destroy', as: 'destroy_book'
 end
