f7d9ec1a9272eb237dafcaacd0b94951171cfc0e
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-07-03 14:02:52 +0000

    [Finish] errorの更新

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   get '/books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => "books#update", as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  get '/books' => 'books#back', as: 'back'
 end
