c65fe60447aa2a56819fff5c8d3e48ff8fbf5177
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-07-03 11:51:39 +0000

    [Update] テスト結果のエラー更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,8 @@ class BooksController < ApplicationController
 def create
   @book = Book.new(book_params)
   if @book.save
-    flash[:notice] = 'Success！'
-    redirect_to book_path(book.id)
+    flash[:notice] = 'successfully'
+    redirect_to book_path(@book.id)
   else
     @books = Book.all
     render :index
@@ -26,8 +26,8 @@ end
 def update
   @book = Book.find(params[:id])
   if @book.update(book_params)
-    flash[:notice] = 'Success！'
-    redirect_to book_path(book.id)
+    flash[:notice] = 'successfully'
+    redirect_to book_path(@book.id)
   else
     @books = Book.all
     render :edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => 'homes#top'
+  get 'home/index'
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/books' => 'books#index'
@@ -7,5 +9,5 @@ Rails.application.routes.draw do
   get '/books/:id' => 'books#show', as: 'book'
   get '/books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => "books#update", as: 'update_book'
-  delete 'lists/:id' => 'books#destroy', as: 'destroy_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
