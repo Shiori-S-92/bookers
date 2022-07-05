51358593a9e97faf43a15fa53d825840f85f2d78
Shiori-S-92 <106877474+Shiori-S-92@users.noreply.github.com>
2022-07-05 17:51:09 +0000

    [Update] routes.rb／resources追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root :to => 'homes#top'
+  # ↓ 課題提出後、resources 追記
+  resources :books, only: [:index, :create]
   get 'home/index'
   get '/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/books' => 'books#index'
-  post '/books' => 'books#create'
-  get '/books' => 'books#index'
+  # get '/books' => 'books#index'
+  # post '/books' => 'books#create'
+  # get '/books' => 'books#index'
   get '/books/:id' => 'books#show', as: 'book'
   get '/books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => "books#update", as: 'update_book'
